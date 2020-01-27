70113676ebd14b5a4f0327263dd05adadbe9733d
songjein <jeinsong100@gmail.com>
2020-01-27 11:11:31 +0000

    [UPDATE] 범용적인 Writer 작성을 위해 Run 인터페이스 변경

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 [미리 정의한 크롤러](https://github.com/go-some/crawler)를 실행시키는 메인 프로그램입니다.
 
 ## 설치
-- 워크 스페이스 어딘가에 적절히 클론해줍니다.
+- 워크스페이스 내에서 go get -u github.com/go-some/executor를 통해 설치
 
 ## 실행
 - 아톰 환경일 경우, ctrl + shift + b
-- 커맨드라인 환경일 경우, go install 후에 바이너리를 직접 실행시켜줍니다 (/bin 아래에 위치) 
+- 커맨드라인 환경일 경우, main.go를 go build 후에 바이너리를 직접 실행
--- main.go
+++ main.go
@@ -1,55 +1,34 @@
 package main
 
 import (
-  "fmt"
-  "crawler"
-  "encoding/csv"
-  "os"
-  "log"
+	"fmt"
+
+	"github.com/go-some/crawler"
 )
 
 type Crawler interface {
-  Run(crawler.Writer)
+	Run(crawler.DocsWriter)
 }
 
 func main() {
-  // 각 사이트의 크롤러를 등록
-  crawlers := []Crawler{
-    &crawler.USToday{},
-    &crawler.MarketWatch{},
-  }
+	// 각 사이트의 크롤러를 등록
+	crawlers := []Crawler{
+		&crawler.MarketWatch{},
+		&crawler.Reuters{},
+		&crawler.USToday{},
+		/* 여기에 추가 해주세요*/
+	}
 
-  fmt.Println("Run Crawler")
+	fmt.Println("Run Crawler")
 
-  // 실제 데이터를 저장소에 쓰는 부분 입니다.
-  // 추후에 데이터베이스로 쓰기 연산을 수행하도록 수정.
-  wtr := func (fName string, docs []crawler.News) {
-    file, err := os.Create(fName)
-    if err != nil {
-      log.Fatalf("Cannot create file %q: %s\n", fName, err)
-      return
-    }
-    defer file.Close()
-    writer := csv.NewWriter(file)
-    defer writer.Flush()
+	// mongoDB writer의 구현체를 얻음
+	// crawler 패키지의 writer.go에 interface DocsWriter를 구현하는 구현체들이 모여 있음
+	wtr := crawler.NewMongoDBWriter()
 
-    writer.Write([]string{"Title", "Body", "Time", "Url", "Origin"})
+	// 크롤러의 실제 구현을 이용해 실행시키는 부분
+	for _, crawler := range crawlers {
+		crawler.Run(wtr)
+	}
 
-    for _, doc := range docs {
-      writer.Write([]string{
-        doc.Title,
-        doc.Body,
-        doc.Time,
-        doc.Url,
-        doc.Origin,
-      })
-    }
-  }
-
-  // 크롤러의 실제 구현을 이용해 실행시키는 부분
-  for _, crawler := range crawlers {
-    crawler.Run(wtr)
-  }
-
-  fmt.Println("Fin Crawler")
+	fmt.Println("Fin Crawler")
 }
