2e7eef5a1299e9483345333f5b2d01e03548258a
songjein <jeinsong100@gmail.com>
2020-01-31 14:36:36 +0000

    [UPDATE] reuse collection handler

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ type Crawler interface {
 func main() {
 	// 각 사이트의 크롤러를 등록
 	crawlers := []Crawler{
-		&crawler.MarketWatch{},
-		&crawler.Reuters{},
+		//&crawler.MarketWatch{},
+		//&crawler.Reuters{},
 		&crawler.USToday{},
 		/* 여기에 추가 해주세요*/
 	}
@@ -24,6 +24,8 @@ func main() {
 	// mongoDB writer의 구현체를 얻음
 	// crawler 패키지의 writer.go에 interface DocsWriter를 구현하는 구현체들이 모여 있음
 	wtr := crawler.NewMongoDBWriter()
+	// connection pool (collection handler) 생성
+	wtr.Init()
 
 	// 크롤러의 실제 구현을 이용해 실행시키는 부분
 	for _, crawler := range crawlers {
@@ -31,4 +33,6 @@ func main() {
 	}
 
 	fmt.Println("Fin Crawler")
+	// connection pool 제거
+	wtr.Destroy()
 }
