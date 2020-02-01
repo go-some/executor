d8d96c4f37c222f7688415214c9105b2be8021f0
songjein <jeinsong100@gmail.com>
2020-02-01 11:51:41 +0000

    [UPDATE] prodcess Init/Destroy's error

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ func main() {
 	// 각 사이트의 크롤러를 등록
 	crawlers := []Crawler{
 		//&crawler.MarketWatch{},
-		//&crawler.Reuters{},
-		&crawler.USToday{},
+		&crawler.Reuters{},
+		&crawler.USAToday{},
 		/* 여기에 추가 해주세요*/
 	}
 
@@ -24,8 +24,13 @@ func main() {
 	// mongoDB writer의 구현체를 얻음
 	// crawler 패키지의 writer.go에 interface DocsWriter를 구현하는 구현체들이 모여 있음
 	wtr := crawler.NewMongoDBWriter()
+
 	// connection pool (collection handler) 생성
-	wtr.Init()
+	err := wtr.Init()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	// 크롤러의 실제 구현을 이용해 실행시키는 부분
 	for _, crawler := range crawlers {
@@ -33,6 +38,11 @@ func main() {
 	}
 
 	fmt.Println("Fin Crawler")
+
 	// connection pool 제거
-	wtr.Destroy()
+	err = wtr.Destroy()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
